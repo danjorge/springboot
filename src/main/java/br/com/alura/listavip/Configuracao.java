b07a0ffce02a7b50b7b05d20eabcdb2062ccd135
Daniel Jorge <daniel.souza@seplag.ce.gov.br>
2018-07-27 11:36:30 -0300

    adding properties file

--- src/main/java/br/com/alura/listavip/Configuracao.java
+++ src/main/java/br/com/alura/listavip/Configuracao.java
@@ -3,12 +3,8 @@
  */
 package br.com.alura.listavip;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
  * @author daniel.souza
@@ -19,16 +15,5 @@ public class Configuracao {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
-	}
-	
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUsername("root");
-		dataSource.setPassword("root");
-		
-		return dataSource;
 	}
 }
